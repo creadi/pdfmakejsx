c1e71fb4effe38d23cccf8d054c62022a771c40c
Anders <idris.maps@gmx.ch>
2019-08-23 08:00:44 +0200

    Fix types hack on build

--- buildScript.js
+++ buildScript.js
@@ -1,13 +1,26 @@
-import { JSXInternal } from './jsx.d'
-export = pdfmakejsx
-export as namespace pdfmakejsx
+const fs = require('fs')
 
 /*
   This technique to get JSX types on the distributed package is inpired by
   https://github.com/preactjs/preact/blob/master/src/index.d.ts
 
-  Difficulty was to get TS to that this is the JSX flavour we are talking about
+  Difficulty was to get TS to understand that this is the JSX flavour we are talking about
 */
+
+
+const jsxTypes = fs.readFileSync('src/jsx.d.ts', 'utf-8')
+fs.writeFileSync(
+  'dist/jsx.d.ts',
+  jsxTypes.split('declare namespace JSX {').join('export namespace JSXInternal {'),
+  'utf-8'
+)
+
+// this part should be updated if API changes
+
+const mainTypes = `
+import { JSXInternal } from './jsx.d'
+export = pdfmakejsx
+export as namespace pdfmakejsx
 
 declare namespace pdfmakejsx {
   export import JSX = JSXInternal
@@ -34,3 +47,9 @@ declare namespace pdfmakejsx {
   }
 
 }
+`
+fs.writeFileSync(
+  'dist/types.d.ts',
+  mainTypes,
+  'utf-8'
+)
